340e932c010326b2648e42e26cb6fbc2ebf9a6cf
echobinod <echobinod@gmail.com>
2023-07-23 15:24:07 +0545

    feat: required components and interfaces added

---
+++ src/app/lib/components/index.ts
@@ -0,0 +1,5 @@
+export * from './footer/footer.component';
+export * from './layout/layout.component';
+export * from './navbar/navbar.component';
+export * from './breadcrumb/breadcrumb.component';
+export * from './logo/logo.component';
